73db664a9d2062b2695bb0077c53c8baeabe70ab
Abner Chou <hi@abnerchou.me>
2020-11-20 17:38:56 -0500

    test new file structure

---
+++ main.js
@@ -0,0 +1,35 @@
+function main() {
+  var getUrlParameter = function getUrlParameter(sParam) {
+      var sPageURL = window.location.search.substring(1),
+          sURLVariables = sPageURL.split('&'),
+          sParameterName,
+          i;
+
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+
+          if (sParameterName[0] === sParam) {
+              return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+          }
+      }
+  };
+
+  var config = 'config.json';
+
+  // Testing code
+  var testconfig = getUrlParameter('testconfig');
+  testconfig && config = testconfig;
+
+  // Actual logic
+  config && $.getJSON(config, function(data) {
+    testconfig && console.log(data);
+    var dest = getUrlParameter('dest');
+    const url = data[dest].url;
+    const author = data[dest].author;
+
+    $.getJSON(url, function(data) {
+      testconfig && console.log(data);
+    });
+
+  });
+}
